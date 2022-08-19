aecf4275d15e71693199fdf2e3d357860455dbc0
KML <kaungmyatlin.worklife@gmail.com>
2022-08-19 12:25:36 +0630

    SSG, RSG compar (un) comnt code 2 c in viewSrcBwsr

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react'
 import useSWR from 'swr'
-const LastSalesPage = () => {
+const LastSalesPage = (props) => {
     // by the time rm for useSWR
-    const [sales, setSales ] = useState();
+    const [sales, setSales ] = useState(props.sales);
     // const [isLoading, setIsLoading] = useState(false);
     // useEffect( () => {
     //     setIsLoading(true)
@@ -31,14 +31,38 @@ const LastSalesPage = () => {
     if (error) {
         return <p> No data yet </p>
     }
-    if (!data || !sales) {
+    if (!data && !sales) {
         return <p> Loading ...</p>
     }
+    // by the time rm bcoz updated
+    // if (!data || !sales) {
+    //     return <p> Loading ...</p>
+    // }
     return (
         <ul>
             {sales.map(sale => <li key={sale.id}> {sale.username} - ${sale.volume} </li>)}
         </ul>
     )
+}
+export async function getStaticProps() {
+    return fetch( 'https://nextcgfiveintossgssrcsr-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json' )
+    .then( resp => resp.json())
+    .then( data => {
+        const tranf = [];
+        for (const key in data) {
+            tranf.push({id: key, username: data[key].username, volume: data[key].volume})
+        }
+        return { props: { sales: tranf}, revalidate: 10 }
+    })
+    // below is the same code but different structure and more recognizable code from previous.
+    // const resp = await fetch( 'https://nextcgfiveintossgssrcsr-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json' )
+    // const data = await resp.json()
+    // const tranf = [];
+    // for (const key in data) {
+    //     tranf.push({id: key, username: data[key].username, volume: data[key].volume})
+    // }
+    // return { props: { sales: tranf}, revalidate: 10 }
+
 }
 
 export default LastSalesPage
